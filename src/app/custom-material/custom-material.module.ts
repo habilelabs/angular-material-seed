16a29dcdbe42bd5820f90e7a1cba85bf60127f8c
Mukesh <mukesh@habilelabs.io>
2017-05-04 13:21:23 +0530

    design change

--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -3,6 +3,6 @@ import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 //add only required modules
 @NgModule({
   imports: [MdButtonModule, MdCheckboxModule],
-  exports: [MdButtonModule, MdCheckboxModule],
+  exports: [MdButtonModule, MdCheckboxModule]
 })
 export class CustomMaterialModule { }
