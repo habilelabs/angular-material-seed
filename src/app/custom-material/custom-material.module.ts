798a887d42d4c7182432ebf8a52b9951e257c29d
Sumit Heda <sumit@habilelabs.io>
2018-09-13 10:09:21 +0530

    Changes: Updated cli version, added angular.json and angular-cli.json removed.

--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -2,10 +2,10 @@
  * this module is intended to be included only required componants from angular material library
  */
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule} from '@angular/material';
 // add only required modules
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatInputModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule]
+  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule]
 })
 export class CustomMaterialModule { }
